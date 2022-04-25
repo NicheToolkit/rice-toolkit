b85b063feabc10c4fe62eecabacbd17987c3380e
Cyan <snow22314@outlook.com>
2022-04-25 17:16:10 +0800

    fix OptionalService optional

--- rice-toolkit-starter/src/main/java/io/github/nichetoolkit/rice/service/OptionalService.java
+++ rice-toolkit-starter/src/main/java/io/github/nichetoolkit/rice/service/OptionalService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @SuppressWarnings("RedundantThrows")
 public interface OptionalService<I,M extends IdModel<I>> extends QueryService<I,M> {
 
-    default void optional(@NonNull IdModel<I> model) throws RestException {}
+    default void optional(@NonNull M model) throws RestException {}
 
     default boolean existById(I id) throws RestException {
         return Optional.ofNullable(this.queryById(id)).isPresent();
