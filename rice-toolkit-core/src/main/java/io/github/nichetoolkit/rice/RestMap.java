85e6072fd2f644149abb77ff73e80be59e217f25
Cyan <snow22314@outlook.com>
2022-04-27 09:45:40 +0800

    fix

--- rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/RestMap.java
+++ rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/RestMap.java
@@ -8,7 +8,6 @@ import java.util.LinkedHashMap;
  * 此类型参数由自定义参数解析器生成，非前端请求参数
  * @author Cyan (snow22314@outlook.com)
  * @version v1.0.0
- * {@link org.springframework.ui.ModelMap}
  */
 public class RestMap extends LinkedHashMap<String, Object> {
 
--- rice-toolkit-starter/src/main/java/io/github/nichetoolkit/rice/interceptor/RiceAuthTokenService.java
+++ rice-toolkit-starter/src/main/java/io/github/nichetoolkit/rice/interceptor/RiceAuthTokenService.java
@@ -1,5 +1,6 @@
 package io.github.nichetoolkit.rice.interceptor;
 
+import io.github.nichetoolkit.rest.RestException;
 import io.github.nichetoolkit.rice.RestMap;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,13 +10,13 @@ import javax.servlet.http.HttpServletRequest;
  * @author Cyan (snow22314@outlook.com)
  * @version v1.0.0
  */
-public interface RiceAuthTokenService<T, R extends RiceLoginResult>  {
+public interface RiceAuthTokenService<T, R extends RiceLoginResult> {
 
-    String generateToken(RestMap restMap, Object login, R loginResult);
+    String generateToken(RestMap restMap, Object login, R loginResult) throws RestException;
 
-    T checkToken(String accessToken);
+    T checkToken(String accessToken) throws RestException;
 
-    T userInfo();
+    T userInfo() throws RestException;
 
-    T userInfo(HttpServletRequest request);
+    T userInfo(HttpServletRequest request) throws RestException;
 }
