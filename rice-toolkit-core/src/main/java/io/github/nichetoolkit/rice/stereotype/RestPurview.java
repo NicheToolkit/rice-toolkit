6fb474935c8c546e3851db859ddaf760f26c6875
Cyan <snow22314@outlook.com>
2022-05-05 13:57:52 +0800

    purview

--- rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/stereotype/RestPurview.java
+++ rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/stereotype/RestPurview.java
@@ -1,16 +1,26 @@
 package io.github.nichetoolkit.rice.stereotype;
 
+import io.github.nichetoolkit.rest.RestArithmetic;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * <p>RestUser 接口认证用户注解</p>
+ * <p>RestPurview</p>
  * @author Cyan (snow22314@outlook.com)
  * @version v1.0.0
  */
-@Target({ElementType.METHOD,ElementType.TYPE,ElementType.PARAMETER})
+@Target({ElementType.METHOD,ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface RestUser {
+@RestUser
+public @interface RestPurview {
+
+    String name() default "";
+
+    long value() default 0L;
+
+    Class<? extends RestArithmetic> type() default RestArithmetic.class;
+
 }
