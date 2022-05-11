4d156232ebe7886cec834acc97852b2dc58e19fe
Cyan <snow22314@outlook.com>
2022-05-11 16:38:19 +0800

    fix

---
+++ rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/serialize/BigDecimalFormat.java
@@ -0,0 +1,25 @@
+package io.github.nichetoolkit.rice.serialize;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>BigDecimalFormat</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @version v1.0.0
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@JacksonAnnotationsInside
+@JsonSerialize(using = BigDecimalSerializer.class)
+@JsonDeserialize(using = NumberDeserializers.BigDecimalDeserializer.class)
+public @interface BigDecimalFormat {
+    String value() default "###,##0.0000";
+}
