b484fd0643e727d55f1ea593844903cab07f8a4c
Cyan <snow22314@outlook.com>
2022-06-09 13:02:20 +0800

    fix

--- rice-toolkit-starter/src/main/java/io/github/nichetoolkit/rice/helper/IdArrayHelper.java
+++ rice-toolkit-starter/src/main/java/io/github/nichetoolkit/rice/helper/IdArrayHelper.java
@@ -3,7 +3,8 @@ package io.github.nichetoolkit.rice.helper;
 import io.github.nichetoolkit.rest.error.ClassUnknownException;
 import io.github.nichetoolkit.rest.error.ClassUnsupportedException;
 import io.github.nichetoolkit.rest.util.GeneralUtils;
-import io.github.nichetoolkit.rice.IdModel;
+import io.github.nichetoolkit.rice.RestId;
+import io.github.nichetoolkit.rice.RestId;
 import io.github.nichetoolkit.rice.clazz.ClazzHelper;
 
 import java.lang.reflect.Array;
@@ -17,7 +18,7 @@ import java.util.List;
 public class IdArrayHelper {
 
     @SuppressWarnings("unchecked")
-    public static<I,M extends IdModel<I>> I id(M model, int index, I... idArray) {
+    public static<I,M extends RestId<I>> I id(M model, int index, I... idArray) {
         I id = null;
         /* 优先通过 ResponseBody 关联id */
         if (GeneralUtils.isNotEmpty(model)) {
@@ -29,7 +30,7 @@ public class IdArrayHelper {
     }
 
     @SuppressWarnings("unchecked")
-    public static<I,M extends IdModel<I>> I id(List<M> models, int index, I... idArray) {
+    public static<I,M extends RestId<I>> I id(List<M> models, int index, I... idArray) {
         I id = null;
         if (GeneralUtils.isNotEmpty(models) && models.size() == 1 ) {
             M model = models.stream().findFirst().get();
@@ -39,7 +40,7 @@ public class IdArrayHelper {
     }
 
     @SuppressWarnings("unchecked")
-    public static <I,M extends IdModel<I>>  void idArray(List<M> models, int index, I... idArray) throws ClassUnknownException, ClassUnsupportedException {
+    public static <I,M extends RestId<I>>  void idArray(List<M> models, int index, I... idArray) throws ClassUnknownException, ClassUnsupportedException {
         /* 当models的数据有且仅有一条不为空的数据 将model的id反向关联 */
         if (GeneralUtils.isNotEmpty(models) && models.size() == 1) {
             M model = models.stream().findFirst().get();
@@ -48,7 +49,7 @@ public class IdArrayHelper {
     }
 
     @SuppressWarnings("unchecked")
-    public static <I,M extends IdModel<I>> void idArray(M model, int index, I... idArray) throws ClassUnknownException, ClassUnsupportedException {
+    public static <I,M extends RestId<I>> void idArray(M model, int index, I... idArray) throws ClassUnknownException, ClassUnsupportedException {
         if (GeneralUtils.isNotEmpty(model)) {
             /*  将model的id反向关联 */
             if (idArray.length > index) {
