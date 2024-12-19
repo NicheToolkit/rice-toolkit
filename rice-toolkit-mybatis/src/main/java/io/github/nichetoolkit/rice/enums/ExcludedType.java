bfec064f82950ebd8609d0c9cd186d867b701f6a
Cyan <snow22314@outlook.com>
2024-12-19 11:40:45 +0800

    Revert "rename upsetType"
    This reverts commit 47bf8716243ca9fa8e2ec6df53da27b978019aca.

--- rice-toolkit-mybatis/src/main/java/io/github/nichetoolkit/rice/enums/ExcludedType.java
+++ rice-toolkit-mybatis/src/main/java/io/github/nichetoolkit/rice/enums/ExcludedType.java
@@ -8,26 +8,26 @@ import io.github.nichetoolkit.rest.RestKey;
 import java.util.Optional;
 
 /**
- * <code>UpsetType</code>
- * <p>The upset type enumeration.</p>
+ * <code>ExcludedType</code>
+ * <p>The excluded type enumeration.</p>
  * @see  io.github.nichetoolkit.rest.RestKey
  * @author Cyan (snow22314@outlook.com)
  * @since Jdk1.8
  */
-public enum UpsetType implements RestKey<String> {
+public enum ExcludedType implements RestKey<String> {
     /**
      * <code>BUILTIN</code>
-     * <p>The builtin upset type field.</p>
+     * <p>The builtin excluded type field.</p>
      */
     BUILTIN(SQLConstants.BUILTIN),
     /**
      * <code>EXCLUDED</code>
-     * <p>The excluded upset type field.</p>
+     * <p>The excluded excluded type field.</p>
      */
     EXCLUDED(SQLConstants.EXCLUDED),
     /**
      * <code>VALUES</code>
-     * <p>The values upset type field.</p>
+     * <p>The values excluded type field.</p>
      */
     VALUES(SQLConstants.VALUES),
     ;
@@ -40,12 +40,12 @@ public enum UpsetType implements RestKey<String> {
     private final String key;
 
     /**
-     * <code>UpsetType</code>
-     * <p>Instantiates a new upset type.</p>
+     * <code>ExcludedType</code>
+     * <p>Instantiates a new excluded type.</p>
      * @param key {@link java.lang.String} <p>The key parameter is <code>String</code> type.</p>
      * @see  java.lang.String
      */
-    UpsetType(String key) {
+    ExcludedType(String key) {
         this.key = key;
     }
 
@@ -61,12 +61,12 @@ public enum UpsetType implements RestKey<String> {
      * @param key {@link java.lang.String} <p>The key parameter is <code>String</code> type.</p>
      * @see  java.lang.String
      * @see  com.fasterxml.jackson.annotation.JsonCreator
-     * @return  {@link io.github.nichetoolkit.rice.enums.UpsetType} <p>The parse key return object is <code>UpsetType</code> type.</p>
+     * @return  {@link io.github.nichetoolkit.rice.enums.ExcludedType} <p>The parse key return object is <code>ExcludedType</code> type.</p>
      */
     @JsonCreator
-    public static UpsetType parseKey(String key) {
-        UpsetType typeEnum = RestKey.parseKey(UpsetType.class, key);
-        return Optional.ofNullable(typeEnum).orElse(UpsetType.BUILTIN);
+    public static ExcludedType parseKey(String key) {
+        ExcludedType typeEnum = RestKey.parseKey(ExcludedType.class, key);
+        return Optional.ofNullable(typeEnum).orElse(ExcludedType.BUILTIN);
     }
 
 }
