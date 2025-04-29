1cd2b294425fab1ef5f3701132be13a812a678a8
Cyan <snow22314@outlook.com>
2025-04-29 11:40:54 +0800

    fix

--- rice-toolkit-mybatis/src/main/java/io/github/nichetoolkit/mybatis/fickle/RestFickleType.java
+++ rice-toolkit-mybatis/src/main/java/io/github/nichetoolkit/mybatis/fickle/RestFickleType.java
@@ -37,7 +37,9 @@ public interface RestFickleType extends RestType, Serializable {
      * <p>The get jdbc type getter method.</p>
      * @return {@link org.apache.ibatis.type.JdbcType} <p>The get jdbc type return object is <code>JdbcType</code> type.</p>
      * @see org.apache.ibatis.type.JdbcType
+     * @see com.fasterxml.jackson.annotation.JsonIgnore
      */
+    @JsonIgnore
     JdbcType getJdbcType();
 
     /**
@@ -45,7 +47,9 @@ public interface RestFickleType extends RestType, Serializable {
      * <p>The get java type getter method.</p>
      * @return {@link com.fasterxml.jackson.databind.JavaType} <p>The get java type return object is <code>JavaType</code> type.</p>
      * @see com.fasterxml.jackson.databind.JavaType
+     * @see com.fasterxml.jackson.annotation.JsonIgnore
      */
+    @JsonIgnore
     default JavaType getJavaType() {
         return TypeFactory.defaultInstance().constructType(getType());
     }
