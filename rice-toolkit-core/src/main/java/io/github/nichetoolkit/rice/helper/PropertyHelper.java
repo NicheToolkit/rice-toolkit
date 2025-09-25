2355f575b1e41c2b6c85d1649a4ac5f4be93ffb9
Cyan <snow22314@outlook.com>
2025-09-25 16:42:10 +0800

    fix

--- rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/helper/PropertyHelper.java
+++ rice-toolkit-core/src/main/java/io/github/nichetoolkit/rice/helper/PropertyHelper.java
@@ -11,8 +11,8 @@ import java.util.stream.Collectors;
 /**
  * <code>PropertyHelper</code>
  * <p>The property helper class.</p>
- * @see  lombok.extern.slf4j.Slf4j
  * @author Cyan (snow22314@outlook.com)
+ * @see lombok.extern.slf4j.Slf4j
  * @since Jdk1.8
  */
 @Slf4j
@@ -22,9 +22,9 @@ public class PropertyHelper {
      * <code>toPropertiesList</code>
      * <p>The to properties list method.</p>
      * @param properties {@link java.lang.String} <p>The properties parameter is <code>String</code> type.</p>
-     * @see  java.lang.String
-     * @see  java.util.List
-     * @return  {@link java.util.List} <p>The to properties list return object is <code>List</code> type.</p>
+     * @return {@link java.util.List} <p>The to properties list return object is <code>List</code> type.</p>
+     * @see java.lang.String
+     * @see java.util.List
      */
     public static List<Property> toPropertiesList(String properties) {
         if (GeneralUtils.isNotEmpty(properties)) {
@@ -41,9 +41,9 @@ public class PropertyHelper {
      * <code>toPropertiesJson</code>
      * <p>The to properties json method.</p>
      * @param properties {@link java.util.Collection} <p>The properties parameter is <code>Collection</code> type.</p>
-     * @see  java.util.Collection
-     * @see  java.lang.String
-     * @return  {@link java.lang.String} <p>The to properties json return object is <code>String</code> type.</p>
+     * @return {@link java.lang.String} <p>The to properties json return object is <code>String</code> type.</p>
+     * @see java.util.Collection
+     * @see java.lang.String
      */
     public static String toPropertiesJson(Collection<Property> properties) {
         if (GeneralUtils.isNotEmpty(properties)) {
@@ -60,27 +60,19 @@ public class PropertyHelper {
         }
     }
 
-
     /**
      * <code>toPropertiesMap</code>
      * <p>The to properties map method.</p>
      * @param properties {@link java.util.Collection} <p>The properties parameter is <code>Collection</code> type.</p>
-     * @see  java.util.Collection
-     * @see  java.util.Map
-     * @return  {@link java.util.Map} <p>The to properties map return object is <code>Map</code> type.</p>
+     * @return {@link java.util.Map} <p>The to properties map return object is <code>Map</code> type.</p>
+     * @see java.util.Collection
+     * @see java.util.Map
      */
-    public static Map<String,String> toPropertiesMap(Collection<Property> properties) {
+    public static Map<String,Object> toPropertiesMap(Collection<Property> properties) {
         if (GeneralUtils.isNotEmpty(properties)) {
             return properties.stream().filter(Objects::nonNull)
-                    .collect(Collectors.toMap(Property::getName,
-                            property -> {
-                                Object value = property.getValue();
-                                if (GeneralUtils.isNotEmpty(value)) {
-                                    return String.valueOf(value);
-                                } else {
-                                    return "";
-                                }
-                            }, (oldValue, newValue) -> newValue, LinkedHashMap::new));
+                    .collect(Collectors.toMap(Property::getName,Property::getValue,
+                            (oldValue, newValue) -> newValue, LinkedHashMap::new));
         } else {
             return Collections.emptyMap();
         }
@@ -90,21 +82,14 @@ public class PropertyHelper {
      * <code>toPropertiesMap</code>
      * <p>The to properties map method.</p>
      * @param properties {@link java.lang.String} <p>The properties parameter is <code>String</code> type.</p>
-     * @see  java.lang.String
-     * @see  java.util.Map
-     * @return  {@link java.util.Map} <p>The to properties map return object is <code>Map</code> type.</p>
+     * @return {@link java.util.Map} <p>The to properties map return object is <code>Map</code> type.</p>
+     * @see java.lang.String
+     * @see java.util.Map
      */
-    public static Map<String,String> toPropertiesMap(String properties) {
+    public static Map<String,Object> toPropertiesMap(String properties) {
         if (GeneralUtils.isNotEmpty(properties)) {
             Map<String, Map<String, Object>> propertiesMap = JsonUtils.parseMapMap(properties, String.class, String.class, Object.class);
-            return propertiesMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,entry -> {
-                Object value = entry.getValue().get(Property.VALUE);
-                if (GeneralUtils.isNotEmpty(value)) {
-                    return String.valueOf(value);
-                } else {
-                    return "";
-                }
-            }));
+            return propertiesMap.entrySet().stream().collect(Collectors.toMap(Map.Entry::getKey,entry -> entry.getValue().get(Property.VALUE)));
         } else {
             return Collections.emptyMap();
         }
@@ -114,11 +99,11 @@ public class PropertyHelper {
      * <code>toPropertiesList</code>
      * <p>The to properties list method.</p>
      * @param propertiesMap {@link java.util.Map} <p>The properties map parameter is <code>Map</code> type.</p>
-     * @see  java.util.Map
-     * @see  java.util.List
-     * @return  {@link java.util.List} <p>The to properties list return object is <code>List</code> type.</p>
+     * @return {@link java.util.List} <p>The to properties list return object is <code>List</code> type.</p>
+     * @see java.util.Map
+     * @see java.util.List
      */
-    public static List<Property> toPropertiesList(Map<String,String> propertiesMap) {
+    public static List<Property> toPropertiesList(Map<String,Object> propertiesMap) {
         if (GeneralUtils.isNotEmpty(propertiesMap)) {
             return propertiesMap.entrySet().stream().map(entry -> Property.builder().name(entry.getKey())
                     .value(entry.getValue())
