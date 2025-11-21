213f7a3d48930ca6d399e08005831e36db8f44f7
Cyan <snow22314@outlook.com>
2025-11-21 18:37:04 +0800

    fix

--- rice-toolkit-mybatis/src/main/java/io/github/nichetoolkit/mybatis/column/RestAlertKey.java
+++ rice-toolkit-mybatis/src/main/java/io/github/nichetoolkit/mybatis/column/RestAlertKey.java
@@ -7,12 +7,12 @@ import java.lang.annotation.*;
 /**
  * <code>RestAlertKey</code>
  * <p>The rest alert key interface.</p>
- * @see  java.lang.annotation.Annotation
- * @see  java.lang.annotation.Retention
- * @see  java.lang.annotation.Target
- * @see  java.lang.annotation.Documented
- * @see  org.springframework.stereotype.Indexed
  * @author Cyan (snow22314@outlook.com)
+ * @see java.lang.annotation.Annotation
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.Target
+ * @see java.lang.annotation.Documented
+ * @see org.springframework.stereotype.Indexed
  * @since Jdk1.8
  */
 @Retention(RetentionPolicy.RUNTIME)
@@ -20,5 +20,12 @@ import java.lang.annotation.*;
 @Documented
 @Indexed
 public @interface RestAlertKey {
+    /**
+     * <code>value</code>
+     * <p>The value method.</p>
+     * @return {@link java.lang.String} <p>The value return object is <code>String</code> type.</p>
+     * @see java.lang.String
+     */
+    String value() default "";
 }
 
